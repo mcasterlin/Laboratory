8e57c08b7b0a865d743b169f4faa2f02eda42d78
Michael Casterlin <mcasterlin@gmail.com>
2015-10-16 15:24:28 -0500

    Retro Achieved.

--- PyFiles/cake.py
+++ PyFiles/cake.py
@@ -2,23 +2,26 @@ import time
 import sys
 import os
 
-with open ("cake.txt", "r") as myfile:
+# Extract contents and save as string, convert to array
+with open (".cake.txt", "r") as myfile:
     data=myfile.read()
 marquee = list(data)
 
+# Variables to keep track of current character in array
 char_count = 0
 HEAD = ''
 
+# Loop which prints chars until end of array, then runs archey
 os.system('clear')
 while(1):
-    time.sleep(0.01)
+    time.sleep(0.005)
     HEAD = marquee[char_count]
     sys.stdout.write(HEAD)
     sys.stdout.flush()
     char_count += 1
 
     if char_count == len(marquee):
-        time.sleep(5)
+        time.sleep(10)
         os.system("archey3")
         time.sleep(10)
         char_count = 0
